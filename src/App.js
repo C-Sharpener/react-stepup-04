e893f6e8281c019561cf5bd2c229ebc9d95b7063
C-Sharpener <56298258+C-Sharpener@users.noreply.github.com>
2022-01-24 00:07:52 +0900

    ネストされたページ遷移

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Page2";
 import "./styles.css";
 
@@ -28,9 +30,34 @@ export default function App() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/page1">
-          <Page1 />
-        </Route>
+        {/* <Route path="/page1"> */}
+        {/* <Page1 /> */}
+        {/* </Route> */}
+        {/* <Route path="/page1" render={() => <Page1 />} /> */}
+        <Route
+          path="/page1"
+          // render={() => (
+          // render={(props) => (
+          render={({ match: { url } }) => (
+            <Switch>
+              {/* {console.log(props)} */}
+              {console.log(url)}
+              {/* <Route exact path="/page1"> */}
+              <Route exact path="url">
+                <Page1 />
+              </Route>
+              {/* <Route path="/page1/detailA"> */}
+              <Route path={`${url}/detailA`}>
+                <Page1DetailA />
+              </Route>
+              {/* <Route path="/page1/detailB"> */}
+              <Route path={`${url}/detailB`}>
+                <Page1DetailB />
+              </Route>
+              <Page1 />
+            </Switch>
+          )}
+        />
         <Route path="/page2">
           <Page2 />
         </Route>
