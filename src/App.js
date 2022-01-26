82d22aa20aab1484e20e3f4446b6c07dbe6bce6d
C-Sharpener <56298258+C-Sharpener@users.noreply.github.com>
2022-01-27 01:20:02 +0900

    Works fine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // import { BrowserRouter } from "react-router-dom";
 // import { BrowserRouter, Link } from "react-router-dom";
 // import { BrowserRouter, Link, Switch } from "react-router-dom";
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+// import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page1DetailA } from "./Page1DetailA";
-import { Page1DetailB } from "./Page1DetailB";
-import { Page2 } from "./Page2";
+import { Router } from "./router/Router";
+// import { Home } from "./Home";
+// import { Page1 } from "./Page1";
+// import { Page1DetailA } from "./Page1DetailA";
+// import { Page1DetailB } from "./Page1DetailB";
+// import { Page2 } from "./Page2";
 import "./styles.css";
 
 export default function App() {
@@ -25,43 +27,7 @@ export default function App() {
         {/* <Page1 /> */}
         {/* <Page2 /> */}
       </div>
-      <Switch>
-        {/* <Route path="/"> */}
-        <Route exact path="/">
-          <Home />
-        </Route>
-        {/* <Route path="/page1"> */}
-        {/* <Page1 /> */}
-        {/* </Route> */}
-        {/* <Route path="/page1" render={() => <Page1 />} /> */}
-        <Route
-          path="/page1"
-          // render={() => (
-          // render={(props) => (
-          render={({ match: { url } }) => (
-            <Switch>
-              {/* {console.log(props)} */}
-              {/* {console.log(url)} */}
-              {/* <Route exact path="/page1"> */}
-              <Route exact path="url">
-                <Page1 />
-              </Route>
-              {/* <Route path="/page1/detailA"> */}
-              <Route path={`${url}/detailA`}>
-                <Page1DetailA />
-              </Route>
-              {/* <Route path="/page1/detailB"> */}
-              <Route path={`${url}/detailB`}>
-                <Page1DetailB />
-              </Route>
-              <Page1 />
-            </Switch>
-          )}
-        />
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
